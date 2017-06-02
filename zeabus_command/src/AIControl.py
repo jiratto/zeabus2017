59fa8992fb4595b5f0814ef1fb54ff52366c0666
eveem <monpriya.tammavong@gmail.com>
2017-06-02 17:51:02 +0700

    Add comment and some method

--- zeabus_command/src/AIControl.py
+++ zeabus_command/src/AIControl.py
@@ -11,35 +11,48 @@ from controller.srv import drive_x
 
 class AIControl():
 	def __init__ (self):
-		self.isFixPostion = True
-		self.err = 0.1
-		self.pose = Pose()
-		self.auvState = [0, 0, 0, 0, 0, 0]
-		self.stopTurn = True
+		self.isFixPostion = True 			# use in fix_position call back function
+		self.err = 0.1						# error constant
+		self.pose = Pose()					# pose varible type Pose
+		self.auvState = [0, 0, 0, 0, 0, 0]	# x, y, z in linear and angular of auv from /auv/ 
+		self.stopTurn = True 				# boolean to check turn state subscribe from /controller/is_at_fix_orientation
 
-		rospy.Subscriber ('/auv/state', Odometry, self.set_position)
-		rospy.Subscriber ('/controller/is_at_fix_position', Bool, self.fix_position)
-		rospy.Subscriber ('/controller/is_at_fix_orientation', Bool, self.check_turn)
+		rospy.Subscriber ('/auv/state', Odometry, self.set_position)						# subscribe from /auv/state call back to set_position method
+		rospy.Subscriber ('/controller/is_at_fix_position', Bool, self.fix_position)		# subscribe to check Did you reach the position that you want ?
+		rospy.Subscriber ('/controller/is_at_fix_orientation', Bool, self.check_turn)		# subscribe to check turn state 
 
-		self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size = 10)
-		self.zAxisNow = rospy.Publisher ('/fix/abs/depth', Float64, queue_size = 10)
-		self.fixPoint = rospy.Publisher ('/cmd_fix_position', Point, queue_size = 10)
-		self.turnYawRelative = rospy.Publisher ('/fix/rel/yaw', Float64, queue_size = 10)
-		self.turnYawAbsolute = rospy.Publisher ('/fix/abs/yaw', Float64, queue_size = 10)
+		self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size = 10)					# publish twist x, y, z linear and angular to /cmd_vel
+		self.zAxisNow = rospy.Publisher ('/fix/abs/depth', Float64, queue_size = 10)		# publish z to fix depth
+		self.fixPoint = rospy.Publisher ('/cmd_fix_position', Point, queue_size = 10)		# publish x, y, z
+		self.turnYawRelative = rospy.Publisher ('/fix/rel/yaw', Float64, queue_size = 10)	# publish turn angle relative mode
+		self.turnYawAbsolute = rospy.Publisher ('/fix/abs/yaw', Float64, queue_size = 10)	# publish turn angle absolute mode
 
-		rospy.wait_for_service ('fix_rel_x_srv')
-		self.driveXService = rospy.ServiceProxy ('fix_rel_x_srv', drive_x)
-		self.wait_for_subscriber ()
+		rospy.wait_for_service ('fix_rel_x_srv')							# turn yaw relative by service
+		self.driveXService = rospy.ServiceProxy ('fix_rel_x_srv', drive_x)	# drive x [forward or backword] by service
+		self.wait_for_subscriber ()											# method check subscribtion
 
+	# subscribe from /auv/state
 	def set_position (self, data):
+		self.pose = data.pose.pose
 		pose = data.pose.pose
+		temp = (pose.orientation.y, pose.orientation.z, pose.orientation.w)
+		euler_angular = tf.transformations.euler_from_quaternion (temp)
+		self.auvState[0] = pose.position.x
+		self.auvState[1] = pose.position.y
+		self.auvState[2] = pose.position.z
+		self.auvState[3] = euler_angular[0]
+		self.auvState[4] = euler_angular[1]
+		self.auvState[5] = euler_angular[2]
 
+	# call back check fix position
 	def fix_position (self, data):
 		self.isFixPostion = data.data
 
+	# call back check finish of turn
 	def check_turn (self, data):
 		self.stopTurn = data.data
 
+	# check connection
 	def wait_for_subscriber (self, check_interval = 0.3):
 		finish = False
 		while not rospy.is_shutdown () and not finish:
@@ -65,3 +78,25 @@ class AIControl():
 				print 'Some subscribtion not connected ;____;'
 
 		print 'All subscribe complete !'
+
+	# convert list to twist
+	def list_to_twist (self, list):
+		temp = Twist()
+		temp.linear.x = list[0]
+		temp.linear.y = list[1]
+		temp.linear.z = list[2]
+		temp.angular.x = list[3]
+		temp.angular.y = list[4]
+		temp.angular.z = list[5]
+		return temp
+
+	# publish to cmd_vel
+	def published (self, tw):
+		print 'linear x:%f y:%f z:%f'%(tw.linear.x, tw.linear.y, tw.linear.z)
+		print 'angular x:%f y:%f z:%f'%(tw.angular.x, tw.angular.y, tw.angular.z)
+
+		self.command.publish (tw)
+		rospy.sleep (0.05)
+
+	def get_position (self): 	
+		return self.auvState
