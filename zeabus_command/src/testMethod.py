616ab12fc334b4144f1062a7a23a9481225f05bd
RocKIn0X <inman27@hotmail.com>
2017-06-09 23:35:43 +0700

    Edit callback set_position

--- zeabus_command/src/testMethod.py
+++ zeabus_command/src/testMethod.py
@@ -5,45 +5,50 @@ import math
 from AIControl import AIControl
 
 if __name__ == '__main__':
+	print 'HELLO'
 	rospy.sleep (2)
 	aicontrol = AIControl ()
 
-	aicontrol.drive_xaxis (5)
-	aicontrol.drive_xaxis (-5)
-	rospy.sleep (0.5)
+	rospy.init_node ('testMethod')
+	print 'Init complete'
 
-	aicontrol.drive_yaxis (5)
-	aicontrol.drive_xaxis (-5)
-	rospy.sleep (0.5)
+	# aicontrol.drive_xaxis (5)
+	# aicontrol.drive_xaxis (-5)
+	# rospy.sleep (0.5)
 
-	aicontrol.drive_zaxis (-1)
-	rospy.sleep (0.5)
+	# aicontrol.drive_yaxis (5)
+	# aicontrol.drive_xaxis (-5)
+	# rospy.sleep (0.5)
 
-	aicontrol.drive_xyaxis (1, 1, -1)
-	rospy.sleep (0.5)
+	# aicontrol.drive_zaxis (3)
+	# rospy.sleep (0.5)
+	# print 'drive z la na eiei'
 
-	aicontrol.go_to_xyz (3, 3, -3)
-	rospy.sleep (0.5)
+	# aicontrol.go_to_xyz (3, 3, -3)
+	# rospy.sleep (0.5)
 
-	aicontrol.drive ([1, 1, 1, 0, 0, 0])
-	rospy.sleep (0.5)
+	# aicontrol.drive ([1, 1, 1, 0, 0, 0])
+	# rospy.sleep (0.5)
 
-	aicontrol.turn_yaw_relative (15)
-	aicontrol.turn_yaw_relative (-15)
-	rospy.sleep (0.5)
+	# aicontrol.turn_yaw_relative (15)
+	# aicontrol.turn_yaw_relative (-15)
+	# rospy.sleep (0.5)
 
-	aicontrol.turn_yaw_absolute (25)
-	aicontrol.turn_yaw_absolute (-25)
-	rospy.sleep (0.5)
+	# aicontrol.turn_yaw_absolute (25)
+	# aicontrol.turn_yaw_absolute (-25)
+	# rospy.sleep (0.5)
 
-	aicontrol.turn_yaw (30)
-	rospy.sleep (0.5)
+	# aicontrol.turn_yaw (30)
+	# rospy.sleep (0.5)
 
-	aicontrol.stop (5)
+	# aicontrol.stop (5)
 
-	aicontrol.goto (10, 10, -2, 1)
-	rospy.sleep (0.5)
+	# aicontrol.goto (10, 10, -2, 1)
+	# rospy.sleep (0.5)
 
-	while True:
-		aicontrol.stop (1)
-		rospy.sleep (0.1)
+	# i = 0.1
+	# while i < 10:
+	# 	aicontrol.stop (1)
+	# 	rospy.sleep (0.1)
+	# 	print 'STOP !!'
+	# 	i += 0.1
