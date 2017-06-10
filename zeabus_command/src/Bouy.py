4f8e74a7f2c7c58e1f86295ae5d0571ae45838db
RocKIn0X <inman27@hotmail.com>
2017-06-10 21:45:06 +0700

    init class Bouy

---
+++ zeabus_command/src/Bouy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64, Bool, String
+from AIControl import AIControl
+
+class Bouy (object):
+
+	def __init__ (self):
+        print "Start Mission Bouy"
+
+        rospy.init_node ('bouy_node')
+
+        self.aicontrol = AIControl ()
+        self.ball = 0
+		self.data = None
+		self.leftColor = None
+		self.state = 1
+        self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size=10)
+        self.turn_yaw_rel = rospy.Publisher ('/fix/rel/yaw', Float64, queue_size=10)
+        
+        # bouy_srv = 'bouy'
+        # rospy.wait_for_service (bouy_srv)
+        # self.detect_bouy = rospy.ServiceProxy (bouy_srv, bouy_sim)
