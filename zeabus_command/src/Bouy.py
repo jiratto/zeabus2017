aec21dc0c3c6c5023dcfc077dbe740780fa2c214
eveem <monpriya.tammavong@gmail.com>
2017-06-10 16:25:08 +0700

    Add method find

--- zeabus_command/src/Bouy.py
+++ zeabus_command/src/Bouy.py
@@ -7,20 +7,23 @@ from std_msgs.msg import Float64, Bool, String
 from AIControl import AIControl
 
 class Bouy (object):
-
+	
 	def __init__ (self):
-        print "Start Mission Bouy"
+		print "Start Mission Bouy"
 
-        rospy.init_node ('bouy_node')
+		rospy.init_node ('bouy_node')
 
-        self.aicontrol = AIControl ()
-        self.ball = 0
+		self.aicontrol = AIControl ()
+		self.ball = 0
 		self.data = None
 		self.leftColor = None
 		self.state = 1
-        self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size=10)
-        self.turn_yaw_rel = rospy.Publisher ('/fix/rel/yaw', Float64, queue_size=10)
-        
-        # bouy_srv = 'bouy'
-        # rospy.wait_for_service (bouy_srv)
-        # self.detect_bouy = rospy.ServiceProxy (bouy_srv, bouy_sim)
+		self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size=10)
+		self.turn_yaw_rel = rospy.Publisher ('/fix/rel/yaw', Float64, queue_size=10)
+		
+		# bouy_srv = 'bouy'
+		# rospy.wait_for_service (bouy_srv)
+		# self.detect_bouy = rospy.ServiceProxy (bouy_srv, bouy_sim)
+	
+	def find (self):
+		bouy_data = self.detect_bouy
