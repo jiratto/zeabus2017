31babcdc0ab2fdc321c6489931ff60a6e08298c9
Zeabus <monpriya.tammavong@gmail.com>
2017-06-23 14:49:07 +0700

    Test Method

--- zeabus_command/src/testMethod.py
+++ zeabus_command/src/testMethod.py
@@ -14,27 +14,28 @@ if __name__ == '__main__':
 
 	print 'Forward'
 	aicontrol.drive_xaxis (1)
-	rospy.sleep (1)
+	rospy.sleep (3)
 
 	print 'Backward'
 	aicontrol.drive_xaxis (-1)
-	rospy.sleep (1)
+	rospy.sleep (3)
 
 	print 'Slide left'
 	aicontrol.drive_yaxis (1)
-	rospy.sleep (1)
+	rospy.sleep (3)
 
 	print 'Slide right'
 	aicontrol.drive_yaxis (-1)
-	rospy.sleep (1)	
+	rospy.sleep (3)	
 
 	print 'Fix Z to 3'
 	aicontrol.fix_zaxis (-3)
-	rospy.sleep (1)
 
 	print 'Fix Z to 0'
 	aicontrol.fix_zaxis (-0.2)
-	rospy.sleep (1)
+
+	print 'Fix Z to 1'
+	aicontrol.fix_zaxis (-1)	
 
 	print 'Turn yaw relative 45'
 	aicontrol.turn_yaw_relative (45)
