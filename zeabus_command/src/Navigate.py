747e5ee4962dc2678b25aedcd864ce4422d17d44
eveem <monpriya.tammavong@gmail.com>
2017-06-24 13:16:15 +0700

    Add Navigate file

--- zeabus_command/src/Navigate.py
+++ zeabus_command/src/Navigate.py
@@ -26,48 +26,56 @@ class Navigate (object):
 		mission = 'navigate'
 		req = 'Yeah'
 
+		self.aicontrol.fix_zaxis (-3)
+
 		while not rospy.is_shutdown ():
-			self.data = self.detectNav (String (mission), String (req))
-			self.data = self.data.data
+			self.aicontrol.stop (1)
 
-			## get data ##
-			cx = self.data.cx
-			cy = self.data.cy
-			verticalX = self.data.verticalX
-			verticalY = self.data.verticalY
-			area = self.data.ratioArea
-			numVertical = self.data.numVertical
+			zero = 0
+			one = 0
+			two = 0
+			mul = 0
+			cx = list()
+			cy = list()
+			area = list()
 
-			print ('numVertical: ', numVertical)
-			print ('cx: ', cx)
-			print ('cy: ', cy)
-			print ('verticalX: ', verticalX)
-			print ('verticalY: ', verticalY)
-			print ('ratio area: ', area)
+			for i in xrange (10):
+				self.data = self.detectNav (String (mission), String (req))
+				self.data = self.data.data
 
-			if numVertical >= 2:
-				if self.aicontrol.is_center ([cx, cy], -0.2, 0.2, -0.1, 0.1):
-					if area < 0.4:
-						self.aicontrol.drive.xaxis (0.7)
-					elif area < 0.7:
-						self.aicontrol.drive.xaxis (0.3)
-					elif area < 1:
-						self.aicontrol.drive.xaxis (0.2)
-					else:
-						self.aicontrol.drive.xaxis (0.1)
+				if self.data.numVertical >= 2:
+					two += 1
+					cx[2] += self.data.cx
+					cy[2] += self.data.cy
+					area[2] += self.data.area
+				elif self.data.numVertical == 1:
+					one += 1
+					area[1] += self.data.area
+					mul += self.data.direction
+				elif self.data.numVertical == 0:
+					zero += 1
+
+			if two > one >= zero:
+				print 'TWO LEGS'
+				if self.aicontrol.is_center ([cx[2]/two, cy[2]/two], -0.2, 0.2, -0.1, 0.1):
+					print 'Center !!'
+					self.aicontrol.drive_xaxis (1/area[2]/10)
 				else:
-					vx = self.aicontrol.adjust (cx, -0.5, -0.1, 0.1, 0.5)
-					vy = self.aicontrol.adjust (cy, -0.5, -0.1, 0.1, 0.5)
-					self.aicontrol.drive ([0, vx, 0, 0, 0, 0])
-				rospy.sleep (2)
-			elif numVertical == 1:
-				while not verticalX == -999 and not verticalY == -999:
-					vx = self.aicontrol.adjust (verticalX, -0.5, -0.1, 0.1, 0.5)
-					vy = self.aicontrol.adjust (verticalY, -0.5, -0.1, 0.1, 0.5)
-					self.aicontrol.drive ([0, vx, 0, 0, 0, 0])
-					rospy.sleep (2)
-			elif numVertical == 0:
-				print "MISSION COMPLETE"
+					print 'Drive to center'
+					vx = self.aicontrol.adjust (cx[2]/two, -0.5, -0.1, 0.1, 0.5)
+					vy = self.aicontrol.adjust (cy[2]/two, -0.5, -0.1, 0.1, 0.5)
+					self.aicontrol.drive ([0, vx, vy, 0, 0, 0])
+				rospy.sleep (0.5)
+			elif one > two >= zero:
+				print 'ONE LEG'
+				self.aicontrol.drive_yaxis (0.3 * mul)
+				rospy.sleep (0.5)
+			elif zero > one >= two:
+				print 'Ja roll la na'
+				self.aicontrol.drive_xaxis (1)
+				rospy.sleep (1)
+				self.aicontrol.roll (2)
+				print 'MISSION COMPLETE'
 				break
 			self.aicontrol.stop (1)
 
