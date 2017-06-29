c9e689785b6b563db2bcffc6bd2a4846fbce3b98
buddhinai.k@ku.th <buddinai.k@ku.th>
2017-06-29 22:39:33 +0700

    test bin bag

--- zeabus_vision/zeabus_vision_main/src/color_range_main.py
+++ zeabus_vision/zeabus_vision_main/src/color_range_main.py
@@ -146,7 +146,10 @@ def callback(msg):
         arr = np.fromstring(msg.data, np.uint8)
         img = cv2.imdecode(arr, 1)
         img = cv2.resize(img, (320, 256))
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hsv = equalization(img)
+        # hsv = clahe(fuck)
+        # hsv = equalization(img)
 
 
 def draw_circle(event, x, y, flags, param):
