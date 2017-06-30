1799fe2f08515b58d1d49f2c341623a7e8855b19
Zeabus <monpriya.tammavong@gmail.com>
2017-06-30 15:23:44 +0700

    Add binn mission

---
+++ zeabus_command/src/Binn.py
@@ -0,0 +1,90 @@
+#!usr/bin/env python
+
+import rospy
+import math
+from geometry_msg.msg import Twist
+from std_msgs.msg import Float64, Bool, String
+from zeabus_vision_srv_msg.msg import vision_msg_default
+from zeabus_vision_srv_msg.srv import vision_srv_default
+from AIControl import AIControl
+from Hardware import Hardware
+import Depth as depth
+import Direction as Direction
+
+class Binn (object):
+	def __init__ (self):
+		print 'Start Bin Mission'
+
+		rospy.init_node ('bin_node_init')
+
+		self.aicontrol = AIControl ()
+		self.hardware = Hardware ()
+
+		rospy.wait_for_service ('vision')
+		self.detect_bin = rospy.ServiceProxy ('vision', vision_srv_default)
+
+		self.isFail = 30
+
+	def nocover (self):
+		self.aicontrol.fix_zaxis (depth.BINN_DETECTING)
+		while not rospy.is_shutdown () and not self.aicontrol.is_fail (self.isFail):
+			ax = []
+			ay = []
+			aarea = []
+			aangle = []
+			count = 0
+			found = True
+
+			for i in xrange (10):
+				print 'Collect data to avg'
+				self.data = self.detect_bin (String ('bin'), String ('nocover'))
+				self.data = self.data.data
+
+				if self.data.appear:
+					ax.append (self.data.x)
+					ay.append (self.data.y)
+					aarea.append (self.data.area)
+					aangle.append (self.data.angle)
+					count += 1
+
+				rospy.sleep (0.1)
+
+			if count == 0:
+				found = False
+
+			x = self.aicontrol.average (ax)
+			y = self.aicontrol.average (ay)
+			area = self.aicontrol.average (aarea)
+			angle = self.aicontrol.average (aangle)	
+
+			if found:
+				print 'Found Binn'		
+
+				if self.aicontrol.is_center ([x, y], -0.2, 0.2, -0.1, 0.1):
+					self.aicontrol.stop (1)
+					self.aicontrol.turn_yaw_relative (angle)
+					self.aicontrol.fix_zaxis (depth.BINN_FIRE)
+
+					self.aicontrol.drive_yaxis (0.5)
+					rospy.sleep (1)
+					self.hardware.command (String ('drop_right'), String ('fire'))
+
+					self.aicontrol.drive_yaxis (0.5)
+					rospy.sleep (2)
+					self.hardware.command (String ('drop_left'), String ('fire'))
+				else:
+					x = self.aicontrol.adjust (x, -0.5, -0.1, 0.1, 0.5)
+					y = self.aicontrol.adjust (y, -0.5, -0.1, 0.1, 0.5)
+					self.aicontrol.drive ([x, y, 0, 0, 0, 0])
+					rospy.sleep (2)
+
+	def run (self):
+		self.nocover ()
+
+if __name__ == '__main__':
+	binn = Binn ()
+	binn.run ()
+	print 'FINISH'
+
+
+
