d2c9784f1809d2ae8e9bdcd84a2f8a110c820245
skconan <supakit.kr@gmail.com>
2017-07-04 13:14:50 +0700

    created call_service file

---
+++ zeabus_vision/zeabus_vision_main/src/call_service.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import cv2
+import rospy
+from zeabus_vision_srv_msg.msg import *
+from zeabus_vision_srv_msg.srv import *
+from std_msgs.msg import String
+if __name__ == '__main__':
+    rospy.init_node('call_service')
+    serviceName = 'vision_navigate'
+    print('wait service')
+    rospy.wait_for_service(serviceName)
+    print('service start')
+
+    call = rospy.ServiceProxy(serviceName, vision_srv_navigate)
+    while not rospy.is_shutdown():
+        res = call(String('Navigate'), String('top'))
+        print res
+        rospy.sleep(0.1)
