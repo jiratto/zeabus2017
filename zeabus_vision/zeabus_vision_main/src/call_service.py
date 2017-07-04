937743d3ca8a88a732cd2778292d33f0749c0564
skconan <supakit.kr@gmail.com>
2017-07-04 23:33:01 +0700

    for debug

--- zeabus_vision/zeabus_vision_main/src/call_service.py
+++ zeabus_vision/zeabus_vision_main/src/call_service.py
@@ -6,13 +6,15 @@ from zeabus_vision_srv_msg.srv import *
 from std_msgs.msg import String
 if __name__ == '__main__':
     rospy.init_node('call_service')
+    # serviceName = 'vision_bouy'
     serviceName = 'vision_navigate'
     print('wait service')
     rospy.wait_for_service(serviceName)
     print('service start')
 
     call = rospy.ServiceProxy(serviceName, vision_srv_navigate)
+    # call = rospy.ServiceProxy(serviceName, vision_srv_bouy)
     while not rospy.is_shutdown():
-        res = call(String('Navigate'), String('top'))
+        res = call(String('Navigate'), String('bot'))
         print res
         rospy.sleep(0.1)
